c9f33f9e9b6e16db7da4c2663326de1200b7e887
fury36 <40300926+fury36@users.noreply.github.com>
2019-07-01 18:47:38 +0800

    解决netty的冲突

--- yitao-search-service/src/main/java/com/yitao/service/YitaoSearchServiceApplication.java
+++ yitao-search-service/src/main/java/com/yitao/service/YitaoSearchServiceApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class YitaoSearchServiceApplication {
 
     public static void main(String[] args) {
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
         SpringApplication.run(YitaoSearchServiceApplication.class, args);
     }
 
