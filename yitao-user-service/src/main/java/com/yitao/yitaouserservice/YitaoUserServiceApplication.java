320625ca93532b3173fae4454de3477a7dbff596
fury36 <40300926+fury36@users.noreply.github.com>
2019-07-23 11:06:21 +0800

    用户

---
+++ yitao-user-service/src/main/java/com/yitao/yitaouserservice/YitaoUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yitao.yitaouserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YitaoUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YitaoUserServiceApplication.class, args);
+    }
+
+}
