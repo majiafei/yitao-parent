7b3504d46154437c9c124c0555650e56a9409a60
fury36 <40300926+fury36@users.noreply.github.com>
2019-09-06 14:20:58 +0800

    user

---
+++ yitao-cart-service/src/main/java/com/yitao/yitaocartservice/YitaoCartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yitao.yitaocartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YitaoCartServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YitaoCartServiceApplication.class, args);
+    }
+
+}
---
+++ yitao-cart-service/src/test/java/com/yitao/yitaocartservice/YitaoCartServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yitao.yitaocartservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YitaoCartServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
