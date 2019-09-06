a017d3d8c6a4173301d83d44472e08827cc7abc6
fury36 <40300926+fury36@users.noreply.github.com>
2019-09-06 14:23:19 +0800

    cart-webapi

---
+++ yitao-cart-webapi/src/main/java/com/yitao/yitaocartwebapi/YitaoCartWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.yitao.yitaocartwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YitaoCartWebapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YitaoCartWebapiApplication.class, args);
+    }
+
+}
