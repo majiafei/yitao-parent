e0b6cc4b4c7d401da39089342d9449ad4082d734
1571221458@qq.com <majiafei>
2019-09-08 18:35:08 +0800

    orfder

---
+++ yitao-order-service/src/main/java/com/yitao/yitaoorderservice/YitaoOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yitao.yitaoorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YitaoOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YitaoOrderServiceApplication.class, args);
+    }
+
+}
