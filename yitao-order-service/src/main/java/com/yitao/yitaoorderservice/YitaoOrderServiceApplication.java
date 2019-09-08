a12012a53889be3fa5f079d7e54422ff8c96e16a
1571221458@qq.com <majiafei>
2019-09-08 21:38:34 +0800

    删除购物车的商品

--- yitao-order-service/src/main/java/com/yitao/yitaoorderservice/YitaoOrderServiceApplication.java
+++ yitao-order-service/src/main/java/com/yitao/yitaoorderservice/YitaoOrderServiceApplication.java
@@ -4,8 +4,10 @@ import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
+@MapperScan("com.yitao.mapper")
 @EnableDubbo
 public class YitaoOrderServiceApplication {
 
