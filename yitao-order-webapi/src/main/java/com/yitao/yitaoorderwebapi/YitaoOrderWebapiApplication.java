31ad2aa1306070491100799ee0f616b4a092d382
1571221458@qq.com <majiafei>
2019-09-08 18:34:55 +0800

    order

---
+++ yitao-order-webapi/src/main/java/com/yitao/yitaoorderwebapi/YitaoOrderWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.yitao.yitaoorderwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YitaoOrderWebapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YitaoOrderWebapiApplication.class, args);
+    }
+
+}
